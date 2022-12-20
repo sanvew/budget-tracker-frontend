a5a773b71bee1c012398470075ce2b4f0c2a9ef9
sanvew <sanvew@protonmail.com>
2022-12-21 02:40:56 +0400

    UI prototypes of overview, expenses, goals, expenses-details pages

---
+++ prototypes/scss_watch.sh
@@ -0,0 +1,8 @@
+SRC_FILE='styles.scss'
+SRC_DIR='./scss'
+
+OUT_FILE='styles.css'
+OUT_DIR='./output/css/'
+
+sass -I $SRC_DIR --watch --verbose --style compressed \
+ $SRC_DIR/$SRC_FILE $OUT_DIR/$OUT_FILE

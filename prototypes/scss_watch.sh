008d49dd5daa0dde8f3ae7d85e640d2b09ec6034
sanvew <sanvew@protonmail.com>
2023-08-29 01:54:30 +0400

    expense-detail, modal window rework

--- prototypes/scss_watch.sh
+++ prototypes/scss_watch.sh
@@ -5,4 +5,4 @@ OUT_FILE='styles.css'
 OUT_DIR='./output/css/'
 
 sass -I $SRC_DIR --watch --verbose --style compressed \
- $SRC_DIR/$SRC_FILE $OUT_DIR/$OUT_FILE
+    $SRC_DIR/$SRC_FILE $OUT_DIR/$OUT_FILE
